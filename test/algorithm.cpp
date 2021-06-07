77078d265625cca3bec5f428cc27f742ce0ec839
Mike Kaliman <kaliman.mike@gmail.com>
2021-06-06 20:42:36 -0400

    Add more map addition tests

--- test/algorithm.cpp
+++ test/algorithm.cpp
@@ -8,7 +8,7 @@
 
 TEST(AlgorithmTest, std_map_addition) {
     std::vector<int> vec = {-1, 0, 1, 2};
-    mtl::map(vec.begin(), vec.end(), [](auto i) { return ++i; });
+    mtl::map(vec.begin(), vec.end(), [](auto& i) { return ++i; });
 
     ASSERT_EQ(vec[0], 0);
     ASSERT_EQ(vec[1], 1);
@@ -19,6 +19,16 @@ TEST(AlgorithmTest, std_map_addition) {
 TEST(AlgorithmTest, mtl_map_addition) {
     mtl::StaticArray<int, 4> arr = {-1, 0, 1, 2};
     mtl::map(arr.begin(), arr.end(), [](auto i) { return ++i; });
+
+    ASSERT_EQ(arr[0], 0);
+    ASSERT_EQ(arr[1], 1);
+    ASSERT_EQ(arr[2], 2);
+    ASSERT_EQ(arr[3], 3);
+}
+
+TEST(AlgorithmTest, mtl_map_addition_ref) {
+    mtl::StaticArray<int, 4> arr = {-1, 0, 1, 2};
+    mtl::map(arr.begin(), arr.end(), [](auto& i) { return ++i; });
 
     ASSERT_EQ(arr[0], 0);
     ASSERT_EQ(arr[1], 1);
