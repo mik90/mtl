9e9c46ccf49d23d36e0ca9e732c6dbd4c5ddf778
Mike Kaliman <kaliman.mike@gmail.com>
2021-06-12 23:19:17 -0400

    Start on DynArray

--- include/mtl/dyn_array.hpp
+++ include/mtl/dyn_array.hpp
@@ -1,6 +1,9 @@
 #pragma once
 
-#include <type_traits>
+#include "mtl/maybe.hpp"
+#include <cstdlib>
+#include <cstring>
+#include <utility>
 
 namespace mtl {
 /**
@@ -9,8 +12,76 @@ namespace mtl {
 template <typename ValueType>
 class DynArray {
   private:
-    /// @todo use a StaticArray internally and resize it when needed
+    static constexpr std::size_t default_capacity_ = 12;
+    static constexpr std::size_t default_alignment_ = sizeof(std::size_t);
+    // I'd need OwnedPtr<T[]> to use that, i think
+    ValueType* data_;
+    std::size_t size_ = 0;
+    std::size_t capacity_ = default_capacity_;
 
   public:
+    DynArray() {
+        data_ = static_cast<ValueType*>(
+            std::aligned_alloc(default_alignment_, default_capacity_ * sizeof(ValueType)));
+    }
+
+    void set_capacity(std::size_t new_capacity) {
+        if (new_capacity == capacity_) {
+            return;
+        }
+        if (new_capacity < capacity_) {
+            // New capacity is smol-er
+            size_ = capacity_;
+        }
+        // Increase capacity
+        capacity_ = new_capacity;
+        const ValueType* temp = data_;
+        data_ = static_cast<ValueType*>(
+            std::aligned_alloc(default_alignment_, capacity_ * sizeof(ValueType)));
+        std::memmove(data_, temp, size_);
+    }
+
+    DynArray(std::initializer_list<ValueType> list) {
+        data_ = static_cast<ValueType*>(
+            std::aligned_alloc(default_alignment_, default_capacity_ * sizeof(ValueType)));
+
+        auto begin = list.begin();
+        auto end = list.end();
+        // Limit the amount of values copied over if it's greater than the capacity
+        if (list.size() > capacity_) {
+            set_capacity(list.size());
+        }
+
+        std::for_each(begin, end,
+                      [this](const auto& item) { return this->data_[this->size_++] = item; });
+    }
+
+    void push_back(const ValueType& value) {
+        if (size_ >= capacity_) {
+            set_capacity(capacity_ * 2);
+        }
+        data_[size_++] = value;
+    }
+    void push_back(ValueType&& value) {
+        if (size_ >= capacity_) {
+            set_capacity(capacity_ * 2);
+        }
+        data_[size_++] = std::move(value);
+    }
+
+    ValueType& operator[](size_t idx) { return data_[idx]; }
+
+    const ValueType& operator[](size_t idx) const noexcept { return data_[idx]; }
+
+    Maybe<ValueType> maybe_copy_at(size_t idx) const {
+        if (idx > size_) {
+            return None{};
+        }
+
+        return Some{data_[idx]};
+    }
+
+    std::size_t capacity() const noexcept { return capacity_; }
+    std::size_t size() const noexcept { return size_; }
 };
 } // namespace mtl
--- test/dyn_array.cpp
+++ test/dyn_array.cpp
@@ -2,3 +2,51 @@
 #include <gtest/gtest.h>
 
 #include "mtl/dyn_array.hpp"
+
+TEST(DynArrayTest, ctor) {
+    mtl::DynArray<int> arr;
+    ASSERT_EQ(arr.size(), 0);
+}
+
+TEST(DynArrayTest, ctor_init_list) {
+    const auto array = mtl::DynArray<int>({0, 1, 2});
+    ASSERT_EQ(array.size(), 3);
+    ASSERT_EQ(array[0], 0);
+    ASSERT_EQ(array[1], 1);
+    ASSERT_EQ(array[2], 2);
+}
+
+TEST(DynArrayTest, push_back_ensure_size) {
+    auto array = mtl::DynArray<int>();
+    ASSERT_EQ(array.size(), 0);
+
+    array.push_back(0);
+    ASSERT_EQ(array.size(), 1);
+
+    array.push_back(1);
+    ASSERT_EQ(array.size(), 2);
+
+    array.push_back(2);
+    ASSERT_EQ(array.size(), 3);
+
+    ASSERT_EQ(array[0], 0);
+    ASSERT_EQ(array[1], 1);
+    ASSERT_EQ(array[2], 2);
+}
+
+TEST(DynArrayTest, set_capacity) {
+    auto array = mtl::DynArray<int>();
+    array.set_capacity(2);
+    ASSERT_EQ(array.capacity(), 2);
+}
+
+TEST(DynArrayTest, many_elements) {
+    auto array = mtl::DynArray<std::size_t>();
+    for (std::size_t i = 0; i < 1024; ++i) {
+        array.push_back(i);
+    }
+    // The indices should be equal to the values`
+    for (std::size_t i = 0; i < 1024; ++i) {
+        ASSERT_EQ(array[i], i) << "Expected array[" << i << "] was " << array[i];
+    }
+}
