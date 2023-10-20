f4e845e998413fea43eb64dd9d3067c3f4e4c88d
Mike <kaliman.mike@gmail.com>
2023-10-19 20:57:57 -0400

    add more warnings, add copy test

--- mtl/view.hpp
+++ mtl/view.hpp
@@ -4,7 +4,19 @@ namespace mtl {
   template <typename T> class View {
   public:
     View() = delete;
+
     View(T& value) : value_(value) {}
+
+    View(const View<T>& other) : value_(other.value_) {}
+    // Copies of vievs should be cheap enough, moves aren't needed
+    // Really it's just that i can't get this to compile if i do
+    /*
+    std::string my_string = "hello";
+    mtl::View view_a{my_string};
+    mtl::View view_b = std::move(view_a);
+    EXPECT_EQ(view_b, my_string);
+    */
+    View(View<T>&& other) = delete;
 
     View<T>& operator=(const T& other) {
       value_ = other;
@@ -14,6 +26,16 @@ namespace mtl {
       value_ = other.value_;
       return *this;
     }
+
+    // This is needed, evidently? https://stackoverflow.com/a/58780963
+    auto operator==(const View<T>& other) const { return value_ == other.value_; }
+
+    auto operator<=>(const View<T>& other) const { return value_ <=> other.value_; }
+
+    auto operator<=>(T& other) const { return value_ <=> other; }
+
+    T* operator->() { return &value_; }
+    const T& operator->() const { return value_; }
 
   private:
     T& value_;
--- test/test_view.cpp
+++ test/test_view.cpp
@@ -1,8 +1,8 @@
 #include <gmock/gmock.h>
-#include <gtest/gtest.h>
+
+#include <string>
 
 #include "mtl/view.hpp"
-#include "utils.hpp"
 
 TEST(ViewTest, build_and_assign) {
   int my_int = 2;
@@ -28,4 +28,52 @@ TEST(ViewTest, build_const) {
   // Does not comiple
   // my_int_view.get() = 2;
   EXPECT_EQ(my_int, 2);
+}
+
+TEST(ViewTest, compare_views) {
+  int a = 2;
+
+  mtl::View a_view{a};
+
+  int b = 2;
+
+  mtl::View b_view{b};
+
+  bool eq = a_view == b_view;
+  EXPECT_TRUE(eq);
+  bool gt = a_view > b_view;
+  EXPECT_FALSE(gt);
+}
+
+TEST(ViewTest, compare_underlying) {
+  int a = 2;
+
+  mtl::View a_view{a};
+
+  int b = 2;
+
+  bool res = a == b;
+  EXPECT_TRUE(res);
+}
+
+TEST(ViewTest, operator_arrow_overload) {
+  std::string word{"hello"};
+  {
+    mtl::View<const std::string> const_view{word};
+    EXPECT_EQ(const_view->size(), 5);
+  }
+  mtl::View view{word};
+
+  view->clear();
+  EXPECT_TRUE(word.empty());
+
+  EXPECT_TRUE(view->empty());
+}
+
+TEST(ViewTest, build_and_copy) {
+  int my_int = 2;
+
+  mtl::View view_a{my_int};
+  mtl::View view_b{view_a};
+  EXPECT_EQ(view_a, view_b);
 }
